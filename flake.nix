ed98b5f92c45aa8dc6ebdfea739be1f0b7d2e98e
Hippie Hacker <hh@ii.coop>
2023-06-08 11:32:07 +0300

    Not yet working flake

--- flake.nix
+++ flake.nix
@@ -3,25 +3,25 @@
 
   #### OPTION A
   #### Plainly use https://github.com/nix-community/nix-doom-emacs/blob/master/docs/reference.md#flake
-  # inputs = {
-  #   nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  #   nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
-  # };
-  # outputs = { self, nixpkgs, nix-doom-emacs, ... }:
-  # let
-  #   # system = builtins.currentSystem;
-  #   system = "x86_64-linux";
-  #   pkgs = import nixpkgs { inherit system; };
-  #   doom-emacs = nix-doom-emacs.packages.${system}.default.override {
-  #     doomPrivateDir = ./.;
-  #     emacsPackage = pkgs.emacsPgtkNativeComp;
-  #   };
-  # in
-  # {
-  #   devShells.${system}.default = pkgs.mkShell {
-  #     buildInputs = [ doom-emacs ];
-  #   };
-  # };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
+  };
+  outputs = { self, nixpkgs, nix-doom-emacs, ... }:
+  let
+    # system = builtins.currentSystem;
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+    doom-emacs = nix-doom-emacs.packages.${system}.default.override {
+      doomPrivateDir = ./.;
+      emacsPackage = pkgs.emacsPgtkNativeComp;
+    };
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [ doom-emacs ];
+    };
+  };
 
   #### OPTION B
   #### Wrap nix-doom-emacs in each-system
@@ -49,24 +49,27 @@
   #### Simple Flake with overlays
   #### https://github.com/numtide/flake-utils/blob/main/examples/simple-flake/flake.nix
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.simpleFlake {
-      inherit self nixpkgs;
-      name = "iidoom";
-      preOverlays = [
-        (import (builtins.fetchTarball {
-          url = "https://github.com/nix-community/nix-doom-emacs/archive/588ccf37fa9eb9d2ec787b91c989dcd6892983e9.tar.gz";
-          sha256 = "9071dae89261d94bfba012da6ee7747f7fe7ce97b33bd3db095c552d6087a366";
-          # sha256 = "16f571gd1jja3nqffzqnf0b4w3n907n1k01sl3sd8dl7c37p601p";
-        }))
-      ];
-      overlay = ./overlay.nix;
-      # https://github.com/numtide/flake-utils/blob/main/simpleFlake.nix#LL18C1-L19C1
-      # use this to load other flakes overlays to supplement nixpkgs
-      shell = ./shell.nix;
-    };
+  # inputs.flake-utils.url = "github:numtide/flake-utils";
+  # outputs = { self, nixpkgs, flake-utils }:
+  #   flake-utils.lib.simpleFlake {
+  #     inherit self nixpkgs;
+  #     name = "iidoom";
+  #     # preOverlays = [
+  #     #   (import (builtins.fetchTarball {
+  #     #     # url = "https://github.com/nix-community/nix-doom-emacs/archive/master.tar.gz";
+  #     #     # sha256 = "8e0b9c0901999a86763e884b2cbd288f1aba6ef53d4d404f6717222d96ebd3ed";
+  #     #     url = "https://github.com/nix-community/nix-doom-emacs/archive/588ccf37fa9eb9d2ec787b91c989dcd6892983e9.tar.gz";
+  #     #     # sha256 = "9071dae89261d94bfba012da6ee7747f7fe7ce97b33bd3db095c552d6087a366";
+  #     #     sha256 = "16f571gd1jja3nqffzqnf0b4w3n907n1k01sl3sd8dl7c37p601p";
+  #     #   }))
+  #     # ];
+  #     overlay = ./overlay.nix;
+  #     # https://github.com/numtide/flake-utils/blob/main/simpleFlake.nix#LL18C1-L19C1
+  #     # use this to load other flakes overlays to supplement nixpkgs
+  #     shell = ./shell.nix;
+  #   };
 
+  ##### NON WORKING SCRATCH
   # inputs = {
   #   nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   #   nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
--- overlay.nix
+++ overlay.nix
@@ -3,12 +3,14 @@ final: prev:
   # this key should be the same as the simpleFlake name attribute.
   iidoom = {
     # assuming that hello is a project-specific package;
-    # hello = prev.hello;
+    emacs = prev.emacs.override {
 
-    doom-emacs = prev.nix-doom-emacs.override {
-      doomPrivateDir = ./.;
-      emacsPackage = prev.emacsPgtkNativeComp;
     };
+
+    # doom-emacs = prev.nix-doom-emacs.override {
+    #   doomPrivateDir = ./.;
+    #   # emacsPackage = prev.emacsPgtkNativeComp;
+    # };
     # demonstrating recursive packages
     # terraform-providers = prev.terraform-providers;
   };
